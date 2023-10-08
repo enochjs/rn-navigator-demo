fd8a4f255239c2e1390f74dab8bd62a0dc7ba289
枫荷 <fenghe@linkmore.com>
2023-10-08 17:27:37 +0800

    feat(main): navigator demo add

--- src/screens/Home/Details/index.tsx
+++ src/screens/Home/Details/index.tsx
@@ -1,19 +1,17 @@
 import { View, Text, Button } from 'react-native';
 import { NativeStackScreenProps } from '@react-navigation/native-stack';
-import { RootStackParamList } from '@/index';
+import { RootStackParamList } from '@/routers/HomeStack';
 
 type Props = NativeStackScreenProps<RootStackParamList, 'HomeDetail'>;
 
 export default function HomeDetailScreen(props: Props) {
   const { navigation, route } = props;
 
-  console.log('====route', route.params);
-
   return (
     <View className="flex-1 items-center justify-center">
       <Text>detail screens</Text>
       <Text>{route.params.id}</Text>
-      <Button title="Go to Details... Again" onPress={() => navigation.push('HomeDetail', { id: '123' })} />
+      <Button title="Go back" onPress={() => navigation.goBack()} />
     </View>
   );
 }
